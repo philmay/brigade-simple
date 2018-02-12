757c19b04a7aa1e4483ecaf7ec851173b71c9ad5
Phil May <apm050@motorolasolutions.com>
2018-02-12 08:11:28 -0600

    Brigade script and a simple logging JS file.

---
+++ brigade.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// This file is for testing GitHub fetch and execute of
+// non-containerized code.
+
+events.on('exec', (event, project) => {
+	console.log('==> exec event');
+	console.log('---> Brigade "event"\n' + JSON.stringify(event, null, 2));
+	console.log('---> Brigade "project"\n' + JSON.stringify(project, null, 2));
+
+	// Create job for source
+	// TODO: This completely failed with VSTS. 
+	// To get this to work, probably need SSH to VSTS. This basic experiment worked with
+	// the default Brigade project settings pointing to 'github.com/deis/empty-testbed'.
+	// That code is in the brigade-simple directory of this repo. I'm trying it now with
+	// my own GitHub repo.
+	//
+	var nodeJob = new Job('barenode', 'node:8');
+	nodeJob.tasks = [ "pwd", "ls -a", "node log-stuff.js" ];
+	console.log(JSON.stringify(nodeJob, null, 2));
+
+	// Run the non-container node job
+	nodeJob.run().then( (result) => {
+		console.log('Finished barenode job - ' + result);
+	}).catch( (err) => {
+		console.log('Error running barenode job - ' + JSON.stringify(err, null, 2));
+	});
+
+);
---
+++ log-stuff.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// This file just logs crap to the console to test Brigade's 
+// handling of git fetch and run from Azure.
+
+console.log('******* - log-stuff.js');
+console.log('******* - Brigade fetched the repo from GitHub and executed this file.');
+ 
